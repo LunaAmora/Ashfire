a02de9af37855e347bee636eb6488720f770de26
Luna Amora <luna.mberry@gmail.com>
2022-07-23 23:46:37 -0400

    fix proc macro `FlowControl` generic support

--- firelib_macro/src/lib.rs
+++ firelib_macro/src/lib.rs
@@ -3,15 +3,27 @@ use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(FlowControl)]
-pub fn derive_short_circuit(item: TokenStream) -> TokenStream {
+pub fn derive_flow(item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
     let struct_name = &ast.ident;
+    let params = &ast.generics.params;
 
-    let expanded = quote! {
-        impl<T> firelib::FlowControl for #struct_name<T> {}
-        impl<T> FromResidual<ControlFlow<#struct_name<T>, Infallible>> for #struct_name<T> {
-            fn from_residual(residual: ControlFlow<#struct_name<T>, Infallible>) -> Self {
-                <Self as firelib::FlowControl>::from_residual(residual)
+    let expanded = if params.is_empty() {
+        quote! {
+            impl firelib::FlowControl for #struct_name {}
+            impl FromResidual<ControlFlow<#struct_name, Infallible>> for #struct_name {
+                fn from_residual(residual: ControlFlow<#struct_name, Infallible>) -> Self {
+                    <Self as firelib::FlowControl>::from_residual(residual)
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl<#params> firelib::FlowControl for #struct_name<#params> {}
+            impl<#params> FromResidual<ControlFlow<#struct_name<#params>, Infallible>> for #struct_name<#params> {
+                fn from_residual(residual: ControlFlow<#struct_name<#params>, Infallible>) -> Self {
+                    <Self as firelib::FlowControl>::from_residual(residual)
+                }
             }
         }
     };
