f6240e2b4965aca48f7cf5b14301a871a551d8f6
LunaAmora <luna.mberry@gmail.com>
2022-11-20 11:56:10 -0400

    Add more compiler util fns

--- ashfire/src/compiler/utils.rs
+++ ashfire/src/compiler/utils.rs
@@ -1,6 +1,6 @@
 use firelib::lexer::Loc;
 
-use super::program::{Fmt, LazyError};
+use super::program::{Fmt, LazyError, Program};
 
 pub fn err_loc<S: ToString>(error: S, loc: Loc) -> LazyError {
     let error = error.to_string();
@@ -10,4 +10,25 @@ pub fn err_loc<S: ToString>(error: S, loc: Loc) -> LazyError {
 #[track_caller]
 pub fn todo(loc: Loc) -> LazyError {
     err_loc(format!("\n[HERE]  {}", std::panic::Location::caller()), loc)
+}
+
+impl Program {
+    #[track_caller]
+    pub fn todo<S: ToString>(&self, loc: Loc, error: S) -> ! {
+        panic!("{}{}", self.loc_fmt(loc), error.to_string(),)
+    }
+
+    #[track_caller]
+    pub fn info_here<S: ToString>(&self, loc: Loc, info: S) {
+        info!(
+            "{}{}\n[HERE]  {}",
+            self.loc_fmt(loc),
+            info.to_string(),
+            std::panic::Location::caller()
+        )
+    }
+
+    pub fn info<S: ToString>(&self, loc: Loc, info: S) {
+        info!("{}{}", self.loc_fmt(loc), info.to_string())
+    }
 }
