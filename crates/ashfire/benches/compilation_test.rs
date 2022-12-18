5ee0472806eba985ee5d28a2c6fde3e245eab33b
LunaAmora <luna.mberry@gmail.com>
2022-12-18 11:53:23 -0400

    Change benchmarks to use `criterion.rs`

--- crates/ashfire/benches/compilation_test.rs
+++ crates/ashfire/benches/compilation_test.rs
@@ -1,22 +1,17 @@
-#![feature(test)]
-extern crate test;
+use std::{io, path::Path};
 
-#[cfg(test)]
-mod tests {
-    use std::{io, path::Path};
+use ashfire::{compile, target::Target, LANG_FOLDER};
+use criterion::{criterion_group, criterion_main, Criterion};
 
-    use ashfire::{compile, target::Target, LANG_FOLDER};
-    use test::Bencher;
-
-    #[bench]
-    fn bench_wasi(b: &mut Bencher) {
-        let path = Path::new(LANG_FOLDER).join("test.fire");
-        b.iter(|| compile(&path, io::sink(), Target::Wasi));
-    }
-
-    #[bench]
-    fn bench_wasm4(b: &mut Bencher) {
-        let path = Path::new(LANG_FOLDER).join("w4.fire");
-        b.iter(|| compile(&path, io::sink(), Target::Wasm4));
-    }
+fn bench_wasi(c: &mut Criterion) {
+    let path = Path::new(LANG_FOLDER).join("test.fire");
+    c.bench_function("wasi test", |b| b.iter(|| compile(&path, io::sink(), Target::Wasi)));
 }
+
+fn bench_wasm4(c: &mut Criterion) {
+    let path = Path::new(LANG_FOLDER).join("w4.fire");
+    c.bench_function("wasm4 test", |b| b.iter(|| compile(&path, io::sink(), Target::Wasm4)));
+}
+
+criterion_group!(benches, bench_wasi, bench_wasm4);
+criterion_main!(benches);
