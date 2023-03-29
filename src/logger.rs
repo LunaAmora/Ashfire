5e55c1d10e7a83fb94a99017c9b479800afbc2ad
LunaAmora <luna.mberry@gmail.com>
2023-03-29 12:25:23 -0400

    Simplify log formatter

--- src/logger.rs
+++ src/logger.rs
@@ -1,30 +1,13 @@
-use env_logger::{
-    fmt::{Color, Style, StyledValue},
-    Builder,
-};
-use log::Level;
+use env_logger::Builder;
 
 pub fn formatted_builder() -> Builder {
     let mut builder = Builder::new();
 
     builder.format(|f, record| {
         use std::io::Write;
-
-        let mut style = f.style();
-        let level = colored_level(&mut style, record.level());
-
+        let level = f.default_styled_level(record.level());
         writeln!(f, "[{level}] {}", record.args())
     });
 
     builder
-}
-
-fn colored_level(style: &mut Style, level: Level) -> StyledValue<'_, &'static str> {
-    match level {
-        Level::Trace => style.set_color(Color::Magenta).value("TRACE"),
-        Level::Debug => style.set_color(Color::Blue).value("DEBUG"),
-        Level::Info => style.set_color(Color::Green).value("INFO"),
-        Level::Warn => style.set_color(Color::Yellow).value("WARN"),
-        Level::Error => style.set_color(Color::Red).value("ERROR"),
-    }
 }
